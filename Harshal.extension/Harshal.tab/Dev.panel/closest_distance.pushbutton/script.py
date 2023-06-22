75dd0a4f63fa877edf487e45c30fd450c76fc308
hgunjal <128998020+hgunjal@users.noreply.github.com>
2023-06-22 09:21:12 +0200

    Add files via upload

--- Harshal.extension/Harshal.tab/Dev.panel/closest_distance.pushbutton/script.py
+++ Harshal.extension/Harshal.tab/Dev.panel/closest_distance.pushbutton/script.py
@@ -2,21 +2,56 @@
 __title__ = "Distance Calc"
 __doc__ = """______"""
 
-# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
-# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
-# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
+# IMPORTS
 # ==================================================
 # Regular + Autodesk
+
+import clr
+# clr.AddReference('RevitAPI')
+# clr.AddReference('RevitAPIUI')
+
+import sys
+import math
+
+from Autodesk.Revit.DB import *
+# from Autodesk.Revit.DB.__init___parts.DoubleParameterValue import DoubleParameterValue
+# from Autodesk.Revit.DB.__init___parts.GlobalParameter import GlobalParameter
+# from Autodesk.Revit.DB.__init___parts.ParameterType import ParameterType
+from Autodesk.Revit.DB import GlobalParametersManager, Transaction, GlobalParameter, DoubleParameterValue, SpecTypeId
+
+from pyrevit import revit, forms
+
+from Autodesk.Revit.DB import ElementId, GlobalParameter, DoubleParameterValue
+from Autodesk.Revit.UI import TaskDialog
+from System import InvalidOperationException, ArgumentException
+from System.Collections.Generic import HashSet
+
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference('System')
+from System.Collections.Generic import List
+
+clr.AddReference('RevitNodes')
+
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
 import sys
 import math
 from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import GlobalParametersManager, Transaction, GlobalParameter, DoubleParameterValue, SpecTypeId
 
 # pyRevit
 from pyrevit import forms, revit
 
-# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
-# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
-#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
+#VARIABLES
 # ==================================================
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
@@ -24,12 +59,16 @@ app = __revit__.Application
 rvt_year = int(app.VersionNumber)
 
 # CLASSES
-
+# ==================================================
 def feet_to_mm(feet):
     inches = feet * 12
     mm = inches * 25.4
     return mm
 
+def mm_to_feet(mm):
+    # 1 millimeter is equal to 0.00328084 feet
+    feet = mm * 0.00328084
+    return feet
 
 def calculate_distance(point1, point2):
     distance = abs(point2 - point1)
@@ -44,6 +83,7 @@ def find_minimum_value(values):
             min_value = value
 
     return min_value
+
 
 
 # MAIN
@@ -159,3 +199,34 @@ elif Wall_orientation != 1:
 min_distance = find_minimum_value(distance_grids)
 print('min_distance: {}'.format(min_distance))
 
+
+#CREATE GP AND SET IT WITH PREVIUOUS VALUE
+
+def create_new_global_parameter(document, name, value):
+    t = Transaction(document, 'Creating Global Parameters')
+    t.Start()
+
+    # Create a GP with the given name and type Length
+    gp = GlobalParameter.Create(document, name, SpecTypeId.Length)
+
+    gp.SetValue(DoubleParameterValue(value))
+    gpid = gp.Id
+
+    t.Commit()
+
+    return gpid
+
+def mm_to_feet(mm):
+    # 1 millimeter is equal to 0.00328084 feet
+    feet = mm * 0.00328084
+    return feet
+
+
+parameter_name = "Closest_Distance"
+parameter_value = mm_to_feet(min_distance)
+
+# Call the function to create the new global parameter
+global_parameter_id = create_new_global_parameter(doc, parameter_name, parameter_value)
+
+# Print the ID of the created global parameter
+print("Created Global Parameter ID:", global_parameter_id)
