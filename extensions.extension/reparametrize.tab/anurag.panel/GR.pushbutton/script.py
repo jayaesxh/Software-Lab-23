4628a22be6b0d9cb09366177716b333624a11c2c
anurag9063 <129000609+anurag9063@users.noreply.github.com>
2023-08-04 18:17:30 +0200

    Update script.py

--- extensions.extension/reparametrize.tab/anurag.panel/GR.pushbutton/script.py
+++ extensions.extension/reparametrize.tab/anurag.panel/GR.pushbutton/script.py
@@ -9,9 +9,9 @@ __doc__ = """This is a simple tool to filter grids"""
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝
 # """
 from Autodesk.Revit.DB import *
-
-# pyRevit
 from pyrevit import forms
+from Autodesk.Revit.DB import Line, Grid, XYZ, ElementTransformUtils, BoundingBoxXYZ
+from Autodesk.Revit.DB import Transaction
 
 # """
 # ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
@@ -27,8 +27,39 @@ app     =__revit__.Application
 # ╩ ╩╩ ╩╩╝╚╝
 
 # FECs
+# Get wall
+# GET ALL WALLS
+all_walls = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElementIds()
 
-all_grids = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Grids).WhereElementIsNotElementType().ToElementIds()
-print(all_grids)
+for element_id in all_walls:
+    Wall = doc.GetElement(element_id)
 
-# Next Step - need to extract all the grids - Important
+    wep0 = Wall.Location.Curve.GetEndPoint(0)
+    wep1 = Wall.Location.Curve.GetEndPoint(1)
+    print('Wall End Pt 0 (for Wall ID - {}): {}'.format(element_id, wep0))
+    print('Wall End Pt 1 (for Wall ID - {}): {}'.format(element_id, wep1))
+    print('*' * 100)
+
+    start = XYZ(wep0[0], wep0[1], 0)
+    end = XYZ(wep1[0], wep1[1], 0)
+
+    print(start)
+    print(end)
+
+    t = Transaction(doc, 'Create Grid')
+    t.Start()
+
+    # Create the geometry line which the grid locates
+
+    geomLine = Line.CreateBound(start, end)
+
+    # Create a grid using the geometry line
+    lineGrid = Grid.Create(doc, geomLine)
+    if lineGrid is None:
+        raise Exception("Create a new straight grid failed.")
+
+    # Modify the name of the created grid
+    #lineGrid.Name = "A"
+
+    t.Commit()
+    print("Created Grid Successfully")
