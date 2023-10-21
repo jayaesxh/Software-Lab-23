81a2cfaad6a0c15c7f039fc5a342bf88d14deb28
hgunjal <128998020+hgunjal@users.noreply.github.com>
2023-10-21 20:26:54 +0200

    Update script.py

--- Harshal.extension/Harshal.tab/Dev.panel/closest_distance.pushbutton/script.py
+++ Harshal.extension/Harshal.tab/Dev.panel/closest_distance.pushbutton/script.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-__title__ = "Wall-Grid Distance"
+__title__ = "Automated Parameter Creation"
 __doc__ = """______"""
 
 # IMPORTS
 # ==================================================
 # Regular + Autodesk
+
+import json
 
 import clr
 # clr.AddReference('RevitAPI')
@@ -43,6 +45,7 @@ import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
+import re
 import sys
 import math
 from Autodesk.Revit.DB import *
@@ -51,12 +54,13 @@ from Autodesk.Revit.DB import GlobalParametersManager, Transaction, GlobalParame
 # pyRevit
 from pyrevit import forms, revit
 
-#VARIABLES
+# VARIABLES
 # ==================================================
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 app = __revit__.Application
 rvt_year = int(app.VersionNumber)
+
 
 # CLASSES
 # ==================================================
@@ -65,10 +69,12 @@ def feet_to_mm(feet):
     mm = inches * 25.4
     return mm
 
+
 def mm_to_feet(mm):
     # 1 millimeter is equal to 0.00328084 feet
     feet = mm * 0.00328084
     return feet
+
 
 def calculate_distance(point1, point2):
     distance = abs(point2 - point1)
@@ -107,117 +113,80 @@ def find_second_least_value(numbers):
 
     return second_min_value
 
+
+def get_coordinates(element):
+    curve = element.Location.Curve
+    return curve.Origin.X, curve.Origin.Y
+
+
+def calculate_distance(wall_value, grid_value):
+    return abs(wall_value - grid_value)
+
+
+def feet_to_mm(feet_value):
+    # Assuming you have a function to convert feet to mm
+    return feet_value
+
+
+def get_orientation(element):
+    return abs(element.Location.Curve.Direction.Y)
+
+
+def process_grids(grids, orientation, wall_value, wall_orientation):
+    distances = []
+    for grid_id in grids:
+        grid = doc.GetElement(grid_id)
+        grid_value = feet_to_mm(get_coordinates(grid)[0 if orientation == 1 else 1])
+        distance = calculate_distance(wall_value, grid_value)
+        distances.append(distance)
+
+    return find_minimum_value(distances)
+
+
+def extract_element_ids(element_ids):
+    # Convert ElementId objects to string representation
+    element_ids_str = str(element_ids)
+
+    # Extract element IDs between square brackets using regular expression
+    extracted_ids = re.findall(r'\[([0-9]+)\]', element_ids_str)
+
+    # Convert the extracted strings to integers
+    element_ids_int = [int(id_str) for id_str in extracted_ids]
+
+    return element_ids_int
+
+
 # MAIN
 # ==================================================
 
 
-# GET ALL WALLS
+# Specify the path to JSON file containing dictionary A {wall1: grid1, wall2: grid2, wall3: grid2, wall4: grid3}
+json_file_path = r'C:\Users\harsh\OneDrive\Documents\newew\file1.json'
+
+# Open the JSON file and load its contents into a dictionary
+with open(json_file_path, 'r') as file:
+    dictA = json.load(file)
+
+wall_element_ids = []
+
 all_walls = FilteredElementCollector(doc).OfCategory(
     BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElementIds()
-print(all_walls)
-
-print('*' * 50)
-
-# GET ALL GRIDS
 
 all_grids = FilteredElementCollector(doc).OfCategory(
     BuiltInCategory.OST_Grids).WhereElementIsNotElementType().ToElementIds()
-#print(all_grids)
-
-# GRID SORTING
-
-v_grids = []
-h_grids = []
-for element_id in all_grids:
-    # Perform operations on each element ID here
-
-    Grid = doc.GetElement(element_id)
-
-    Grid_orientation = Grid.Curve.Direction.Y
-    if Grid_orientation == -1:
-        grid_o = "V"
-        v_grids.append(element_id)
-    elif Grid_orientation != -1:
-        grid_o = "H"
-        h_grids.append(element_id)
-
-# GET DISTANCE BETWEEN WALL AND GRID
-
-# 1) get wall coordinate and orientation
-
-wall_element_ids = []
-min_distances = []
 
 for element_id in all_walls:
     Wall = doc.GetElement(element_id)
-
-    Wall_x = Wall.Location.Curve.Origin.X
-    print('Wall_p1 for Element ID- {}: {}'.format(element_id, Wall_x))
-    Wall_y = Wall.Location.Curve.Origin.Y
-    print('Wall_p2 for Element ID- {}: {}'.format(element_id, Wall_y))
-
-    Wall_orientation = abs(Wall.Location.Curve.Direction.Y)                          # V==1 & H!=1
-    print('Wall_orientation for Element ID- {}: {}'.format(element_id, Wall_orientation))
-
-# 2) get coordinates of grids (which are parallel to wall)
-
-    distance_grids = []
-
-    if Wall_orientation == 1:
-        for element_ids in v_grids:
-            Grid = doc.GetElement(element_ids)
-            Grid_x = Grid.Curve.Origin.X
-            Grid_y = Grid.Curve.Origin.Y
-
-            Wall_mm_value = feet_to_mm(Wall_x)
-            Grid_mm_value = feet_to_mm(Grid_x)
-
-            distance = calculate_distance(Wall_mm_value, Grid_mm_value)
-            print('Distance(hor) for Element ID- {}: {}'.format(element_ids, distance))
-
-            distance_grids.append(distance)
-
-    elif Wall_orientation != 1:
-        for element_ids in h_grids:
-            Grid = doc.GetElement(element_ids)
-            Grid_x = Grid.Curve.Origin.X
-            Grid_y = Grid.Curve.Origin.Y
-
-            Wall_mm_value = feet_to_mm(Wall_y)
-            Grid_mm_value = feet_to_mm(Grid_y)
-
-            distance = calculate_distance(Wall_mm_value, Grid_mm_value)
-            print('Distance(ver) for Element ID- {}: {}'.format(element_ids, distance))
-
-            distance_grids.append(distance)
-
-    threshold = 0.00001
-    xmin = find_minimum_value(distance_grids)
-    x2min = find_second_least_value(distance_grids)
-
-    if xmin > threshold:
-        min_distance = xmin
-    elif xmin < threshold:
-        min_distance = x2min
-
-    print('min_distance for Element ID- {}: {}'.format(element_id, min_distance))
-    print('*' * 50)
-
+    # print('Wall Element ID- {}'.format(element_id))
     wall_element_ids.append(element_id)
-    min_distances.append(min_distance)
-
+print(wall_element_ids)
 print('*' * 50)
-# Creating the dictionary using a loop
-min_distance_dict = {}
-for i in range(len(wall_element_ids)):
-    element_id = wall_element_ids[i]
-    min_distance = min_distances[i]
-    min_distance_dict[element_id] = min_distance
 
-# Printing the created dictionary
-print(min_distance_dict)
+wall_element_ids_int = extract_element_ids(wall_element_ids)
+print(wall_element_ids_int)
 
-#CREATE GP AND SET IT WITH PREVIOUS VALUE
+number_of_parameters = len(wall_element_ids)
+
 
 def create_new_global_parameter(document, name, value):
     if not GlobalParametersManager.AreGlobalParametersAllowed(document):
@@ -238,18 +207,34 @@ def create_new_global_parameter(document, name, value):
         trans.Commit()
     return gpid
 
-def mm_to_feet(mm):
-    # 1 millimeter is equal to 0.00328084 feet
-    feet = mm * 0.00328084
-    return feet
 
-for x,y in min_distance_dict.items():
-
-    parameter_name = 'Closest_Distance_EID_{}'.format(x)
-    parameter_value = mm_to_feet(y)
+for i in range(number_of_parameters):
+    parameter_name = 'Distance_Parameter_{}'.format(i + 1)
+    parameter_value = 0.0
 
     # Call the function to create the new global parameter
     global_parameter_id = create_new_global_parameter(doc, parameter_name, parameter_value)
 
     # Print the ID of the created global parameter
     print("Created Global Parameter ID:", global_parameter_id)
+
+parameter_id = []
+all_global_parameter_ids = GlobalParametersManager.GetAllGlobalParameters(doc)
+
+for p_id in all_global_parameter_ids:
+    p = doc.GetElement(p_id)
+    # print('GP Name: {}; ID: {}'.format(p.Name, p_id))
+    parameter_id.append(p_id)
+
+parameter_id_int = extract_element_ids(parameter_id)
+print(parameter_id_int)
+
+dictB = {}
+for k, v in zip(wall_element_ids_int, parameter_id_int):
+    dictB[k] = v
+print(dictB)
+
+with open(r'C:\Users\harsh\OneDrive\Documents\newew\dictB.json', 'w') as fp:        #insert file path here
+    json.dump(dictB, fp, indent=4)
+
+print("JSON file created successfully.")
