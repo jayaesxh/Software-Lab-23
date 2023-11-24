c76d22101d9616e549ebded188d7edf5d2652877
anurag9063 <129000609+anurag9063@users.noreply.github.com>
2023-11-24 10:36:12 +0100

    script.py

--- Anurag.extension/testplugin.tab/GR.panel/Outline Grids.pushbutton/script.py
+++ Anurag.extension/testplugin.tab/GR.panel/Outline Grids.pushbutton/script.py
@@ -1,29 +1,54 @@
 # -*- coding: utf-8 -*-
-__title__ = "Outmost Grids"
-__doc__ = """This script is part of YouTube video
-where I explain RevitAPI Parameters and how to work with them.
+__title__ = "Outline Grids"
+__doc__ = """______"""
 
-You can support my channel on:
-www.patreon.com/ErikFrits"""
-
-# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
-# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
-# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
+# IMPORTS
 # ==================================================
 # Regular + Autodesk
+
 import sys
+
+import json
 import math
+import System
+import clr
+from System.Collections.Generic import List
+# clr.AddReference('RevitAPI')
+# clr.AddReference('RevitAPIUI')
+
+import sys
+
 from Autodesk.Revit.DB import *
+# from Autodesk.Revit.DB.__init___parts.DoubleParameterValue import DoubleParameterValue
+# from Autodesk.Revit.DB.__init___parts.GlobalParameter import GlobalParameter
+# from Autodesk.Revit.DB.__init___parts.ParameterType import ParameterType
 from Autodesk.Revit.DB import GlobalParametersManager, Transaction, GlobalParameter, DoubleParameterValue, SpecTypeId
 
-# pyRevit
-from pyrevit import forms, revit
-# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
-# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
-#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
-# ==================================================
+from Autodesk.Revit.DB import ElementId, GlobalParameter, DoubleParameterValue
+from Autodesk.Revit.UI import TaskDialog
 
-#VARIABLES
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference('System')
+from System.Collections.Generic import List
+
+clr.AddReference('RevitNodes')
+
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+from Autodesk.Revit.DB import *
+
+# pyRevit
+
+# VARIABLES
 # ==================================================
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
@@ -31,105 +56,157 @@ app = __revit__.Application
 rvt_year = int(app.VersionNumber)
 
 
-def find_minimum_value(values):
-    min_value = float('inf')  # Initialize with a large value
-
-    for value in values:
-        if value < min_value:
-            min_value = value
-
-    return min_value
-
-def find_maximum_value(values):
-    max_value = float('-inf')  # Initialize with a small value
-
-    for value in values:
-        if value > max_value:
-            max_value = value
-
-    return max_value
-
-
-
 # MAIN
 # ==================================================
 
-
-# GET ALL WALLS
-all_walls = FilteredElementCollector(doc).OfCategory(
-    BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElementIds()
-# print(all_walls)
-
-
-# GET ALL GRIDS
-
-all_grids = FilteredElementCollector(doc).OfCategory(
-    BuiltInCategory.OST_Grids).WhereElementIsNotElementType().ToElementIds()
-#print(all_grids)
-
-# GRID SORTING
-
-v_grids = []
-h_grids = []
-
-for element_id in all_grids:
-    # Perform operations on each element ID here
-
-    Grid = doc.GetElement(element_id)
-
-    Grid_orientation = Grid.Curve.Direction.Y
-    if Grid_orientation == 1 or Grid_orientation == -1:
-        v_grids.append(element_id)
-    elif Grid_orientation != 1 and Grid_orientation != -1:
-        h_grids.append(element_id)
-
-# Get outline grids
-
-# 1) get coordinates of grids
-
-x_coordinates = []
-y_coordinates = []
-
-# 1.1) Get xmin, xmax
-
-for element_ids in v_grids:
-    Grid = doc.GetElement(element_ids)
-    Grid_x = Grid.Curve.Origin.X
-    # print(Grid_x)
-    # print('*'*50)
-    x_coordinates.append(Grid_x)
+def convert_to_flat_dict(input_dict):
+    flat_dict = {}
+    for key, coordinates_list in input_dict.items():
+        for i, coordinates in enumerate(coordinates_list):
+            flat_dict['{}_{}'.format(key, i + 1)] = coordinates
+    return flat_dict
 
 
-xmin = find_minimum_value(x_coordinates)
-xmax = find_maximum_value(x_coordinates)
-print(xmin)
-print(xmax)
-print('*'*50)
+def orientation(p, q, r):
+    """
+    Function to find the orientation of triplet (p, q, r).
+    The function returns the following values:
+    0: Collinear points
+    1: Clockwise points
+    2: Counterclockwise
+    """
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    if val == 0:
+        return 0  # Collinear
+    return 1 if val > 0 else 2  # Clockwise or Counterclockwise
 
-for element_ids in h_grids:
-    Grid = doc.GetElement(element_ids)
-    Grid_y = Grid.Curve.Origin.Y
-    # print(Grid_y)
-    # print('*'*50)
-    y_coordinates.append(Grid_y)
+
+def atan2(y, x):
+    """
+    Function to calculate arctangent of y/x without using numpy.
+    """
+    if x > 0:
+        return arctan(y / x)
+    elif x < 0 and y >= 0:
+        return arctan(y / x) + pi
+    elif x < 0 and y < 0:
+        return arctan(y / x) - pi
+    elif x == 0 and y > 0:
+        return pi / 2
+    elif x == 0 and y < 0:
+        return -pi / 2
+    elif x == 0 and y == 0:
+        return 0
 
 
-ymin = find_minimum_value(y_coordinates)
-ymax = find_maximum_value(y_coordinates)
-print(ymin)
-print(ymax)
-print('*'*50)
+def arctan(x):
+    """
+    Function to calculate arctangent without using numpy.
+    """
+    angle = 0
+    x_squared = x * x
+    divisor = 1
+    term = x / 1
 
-# 1.2) Get corresponding grid to min/max coordinates
+    while term != 0:
+        angle += term
+        divisor += 2
+        term *= -x_squared / divisor
+        divisor += 2
+        term /= divisor
 
-grid_id_xmin = v_grids[x_coordinates.index(xmin)]
-grid_id_xmax = v_grids[x_coordinates.index(xmax)]
+    return angle
 
-print("Element ID corresponding to leftmost grid:", grid_id_xmin)
-print("Element ID corresponding to rightmost grid:", grid_id_xmax)
 
-grid_id_ymin = h_grids[y_coordinates.index(ymin)]
-grid_id_ymax = h_grids[y_coordinates.index(ymax)]
+# Constants
+pi = 3.141592653589793
 
-print("Element ID corresponding to lowermost grid:", grid_id_ymin)
-print("Element ID corresponding to uppermost grid:", grid_id_ymax)
+
+def graham_scan(points):
+    """
+    Function to compute the convex hull of a set of points using the Graham's scan algorithm.
+    """
+    n = len(points)
+    if n < 3:
+        return "Convex hull not possible with less than 3 points."
+
+    # Find the point with the lowest y-coordinate (and leftmost if tied)
+    pivot = min(points, key=lambda point: (point[1], point[0]))
+
+    # Sort the points based on polar angle from the pivot
+    sorted_points = sorted(points, key=lambda point: (atan2(point[1] - pivot[1], point[0] - pivot[0]), point))
+
+    # Initialize the convex hull with the pivot and the first two sorted points
+    hull = [pivot, sorted_points[0], sorted_points[1]]
+
+    for i in range(2, n):
+        while len(hull) > 1 and orientation(hull[-2], hull[-1], sorted_points[i]) != 2:
+            hull.pop()
+        hull.append(sorted_points[i])
+
+    return hull
+
+
+# Specify the path to JSON file containing dictionary A {wall1: grid1, wall2: grid2, wall3: grid2, wall4: grid3}
+file_path_dictA = r'D:\Software Lab Data\Revit_Plug-ins\Anurag.extension\testplugin.tab\Create Dictionary.panel\Create Dictionary.pushbutton\output.json'
+
+# Open the JSON file and load its contents into a dictionary
+with open(file_path_dictA, 'r') as file:
+    dictA = json.load(file)
+
+print(dictA)
+print("Type dictA: ", type(dictA))
+print("dictA: {}".format(';'.join(dictA)))
+
+grid_coordinates = []
+dictC = {}
+
+for wall_id, grid_id in dictA.items():
+    # Get Wall and Grid elements based on their IDs
+    wall = doc.GetElement(ElementId(int(wall_id)))
+    grid = doc.GetElement(ElementId(int(grid_id)))
+
+    gep0 = grid.Curve.GetEndPoint(0)
+    gep1 = grid.Curve.GetEndPoint(1)
+
+    grid_coordinates.append((gep0[0], gep0[1]))
+    grid_coordinates.append((gep1[0], gep1[1]))
+
+    print("Coordinates for Wall {}:".format(grid_id))
+    print("Endpoint 0: ({}, {})".format(gep0[0], gep0[1]))
+    print("Endpoint 1: ({}, {})".format(gep1[0], gep1[1]))
+    print('*' * 50)
+
+    # Create dictC entries
+    dictC[grid_id] = [(round(gep0[0], 3), round(gep0[1], 3)), (round(gep1[0], 3), round(gep1[1], 3))]
+
+print(grid_coordinates)
+
+print(dictC)
+print("Type dictC: ", type(dictC))
+
+rounded_grid_coordinates = [(round(x, 3), round(y, 3)) for x, y in grid_coordinates]
+
+convex_hull = graham_scan(rounded_grid_coordinates)
+
+print("Convex Hull Vertices:", convex_hull)
+matching_grid_ids = []
+matching_grid_coordinates = []
+
+tolerance = 0.0001
+
+for grid_id, coordinates_list in dictC.items():
+    for convex_vertex in convex_hull:
+        for coordinates in coordinates_list:
+            if all(abs(coord - convex_coord) < tolerance for coord, convex_coord in zip(coordinates, convex_vertex)):
+                matching_grid_ids.append(grid_id)
+
+# Remove duplicates by converting to a set and back to a list
+matching_grid_ids = list(set(matching_grid_ids))
+
+# print("Grid IDs with Similar Coordinates to Convex Hull:", matching_grid_ids)
+
+
+print('*' * 50)
+matching_grid_ids_str = [str(grid_id) for grid_id in matching_grid_ids]
+print("Grid IDs with Similar Coordinates to Convex Hull: {}".format(';'.join(matching_grid_ids_str)))
